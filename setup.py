77a1b12e85bcae70b6245d3d0efaefa9db32a0b3
alexander.zap <alexander.zap@axxessio.com>
2021-05-11 14:37:54 +0200

    init speech2text repository

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import setuptools
+
+__package__ = 'speech2text'
+
+setuptools.setup(
+    name='speech2text',
+    version='0.1',
+    description='A simple module for providing speech-to-text functionality',
+    author='',
+    author_email='',
+    url='',
+    packages=setuptools.find_packages(),
+    package_dir={__package__: __package__},
+)
---
+++ speech2text/__init__.py
@@ -0,0 +1,1 @@
+from .speech2text import Speech2Text
